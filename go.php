6b5a60b2336562a10128d1c601155e3c2bc86f6e
Симзиков Павел <sharovik89@yandex.ru>
2014-02-10 15:32:56 +0200

    Task #11266 updated go.php functionality  - updated post.php template

--- go.php
+++ go.php
@@ -1,8 +1,12 @@
 <?php
-$url = isset($_REQUEST['url']) ? $_REQUEST['url'] : '';
-if($url!=''&&!preg_match('#(http?)://#i',$url)){
-    $url="http://".$url;
-}
+
+define('WP_USE_THEMES', false);
+global $wp, $wp_query, $wp_the_query, $wp_rewrite, $wp_did_header;
+require_once( $_SERVER['DOCUMENT_ROOT'] . '/wp-load.php' );
+
+$item_id = isset($_REQUEST['item_id']) ? $_REQUEST['item_id'] : '';
+$url = get_post_meta($item_id, 'url', TRUE);
+
 if(preg_match('#(http?)://\S+[^\s.,>)\];\'\"!?]#i',$url)){
     header('Content-type: text/html; charset=utf-8');
     echo "Перенаправление";
--- templates/post.php
+++ templates/post.php
@@ -37,7 +37,7 @@
 	</tr>
 	<tr>
 		<td>
-			<a href="<?php echo GS_PLUGIN_URL?>go.php?url=<?php echo get_post_meta($post->ID, 'url', TRUE); ?>" target="_blank" >
+			<a href="<?php echo GS_PLUGIN_URL?>go.php?item_id=<?php echo $post->ID; ?>" target="_blank" >
 				<img src="<?php echo GS_PLUGIN_URL?>img/buy.png" alt="Купить <?php echo $post->post_title; ?>" height="25px"/>
 			</a>
 		</td>
